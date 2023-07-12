c658f2a243d2386855e83a66b827442390ecf646
BrightBlack Consultants <ranga@bblack.co.za>
2023-07-12 10:54:40 +0200

    header and footer config

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,79 @@
-<em>&copy; 2022</em>
+<footer class="site-footer">
+<div class="container">
+<div class="row">
+<div class="col-md-9">
+<div class="row">
+<div class="col-md-6">
+<h2 class="footer-heading mb-4">About</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident rerum unde possimus molestias dolorem fuga, illo quis fugiat!</p>
+</div>
+<div class="col-md-3">
+<h2 class="footer-heading mb-4">Navigations</h2>
+<ul class="list-unstyled">
+<li><a href="#">About Us</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Testimonials</a></li>
+<li><a href="#">Contact Us</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<h2 class="footer-heading mb-4">Follow Us</h2>
+<a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+<a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+<a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+<a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+</div>
+</div>
+</div>
+<div class="col-md-3">
+<form action="#" method="post">
+<div class="input-group mb-3">
+<input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Search products..." aria-label="Enter Email" aria-describedby="button-addon2">
+<div class="input-group-append">
+<button class="btn btn-primary text-white" type="button" id="button-addon2">Search</button>
+</div>
+</div>
+</form>
+</div>
+</div>
+<div class="row pt-5 mt-5 text-center">
+<div class="col-md-12">
+<div class="border-top pt-5">
+<p>
+
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | System by <a href="https://bblack.co.za/" target="_blank">BrightBlack</a>
+
+</p>
+</div>
+</div>
+</div>
+</div>
+</footer>
+</div>
+<script src="/assets/js/jquery-3.3.1.min.js"></script>
+<script src="/assets/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="/assets/js/jquery-ui.js"></script>
+<script src="/assets/js/popper.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/owl.carousel.min.js"></script>
+<script src="/assets/js/jquery.stellar.min.js"></script>
+<script src="/assets/js/jquery.countdown.min.js"></script>
+<script src="/assets/js/jquery.magnific-popup.min.js"></script>
+<script src="/assets/js/bootstrap-datepicker.min.js"></script>
+<script src="/assets/js/aos.js"></script>
+<script src="/assets/js/rangeslider.min.js"></script>
+<script src="/assets/js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon='{"rayId":"7e51b98d6951053e","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}' crossorigin="anonymous"></script>
 </body>
+
+<!-- Mirrored from preview.colorlib.com/theme/directoryads/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Jul 2023 14:27:20 GMT -->
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,8 +1,61 @@
-<!doctype html>
-<html>
-<head>
-    <title>CodeIgniter Tutorial</title>
-</head>
+<!DOCTYPE html>
+<html lang="en"><head>
+<title>DirectoryAds &mdash; Colorlib Website Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800" rel="stylesheet">
+<link rel="stylesheet" href="/assets/fonts/icomoon/style.css">
+<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/css/magnific-popup.css">
+<link rel="stylesheet" href="/assets/css/jquery-ui.css">
+<link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/assets/css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="/assets/fonts/flaticon/font/flaticon.css">
+<link rel="stylesheet" href="/assets/css/aos.css">
+<link rel="stylesheet" href="/assets/css/rangeslider.css">
+<link rel="stylesheet" href="/assets/css/style.css">
+<script nonce="f118a95e-80c5-4738-b3b0-37837a605581">(function(w,d){!function(bg,bh,bi,bj){bg[bi]=bg[bi]||{};bg[bi].executed=[];bg.zaraz={deferred:[],listeners:[]};bg.zaraz.q=[];bg.zaraz._f=function(bk){return function(){var bl=Array.prototype.slice.call(arguments);bg.zaraz.q.push({m:bk,a:bl})}};for(const bm of["track","set","debug"])bg.zaraz[bm]=bg.zaraz._f(bm);bg.zaraz.init=()=>{var bn=bh.getElementsByTagName(bj)[0],bo=bh.createElement(bj),bp=bh.getElementsByTagName("title")[0];bp&&(bg[bi].t=bh.getElementsByTagName("title")[0].text);bg[bi].x=Math.random();bg[bi].w=bg.screen.width;bg[bi].h=bg.screen.height;bg[bi].j=bg.innerHeight;bg[bi].e=bg.innerWidth;bg[bi].l=bg.location.href;bg[bi].r=bh.referrer;bg[bi].k=bg.screen.colorDepth;bg[bi].n=bh.characterSet;bg[bi].o=(new Date).getTimezoneOffset();if(bg.dataLayer)for(const bt of Object.entries(Object.entries(dataLayer).reduce(((bu,bv)=>({...bu[1],...bv[1]})),{})))zaraz.set(bt[0],bt[1],{scope:"page"});bg[bi].q=[];for(;bg.zaraz.q.length;){const bw=bg.zaraz.q.shift();bg[bi].q.push(bw)}bo.defer=!0;for(const bx of[localStorage,sessionStorage])Object.keys(bx||{}).filter((bz=>bz.startsWith("_zaraz_"))).forEach((by=>{try{bg[bi]["z_"+by.slice(7)]=JSON.parse(bx.getItem(by))}catch{bg[bi]["z_"+by.slice(7)]=bx.getItem(by)}}));bo.referrerPolicy="origin";bo.src="../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(bg[bi])));bn.parentNode.insertBefore(bo,bn)};["complete","interactive"].includes(bh.readyState)?zaraz.init():bg.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
 <body>
+<div class="site-wrap">
+<div class="site-mobile-menu">
+<div class="site-mobile-menu-header">
+<div class="site-mobile-menu-close mt-3">
+<span class="icon-close2 js-menu-toggle"></span>
+</div>
+</div>
+<div class="site-mobile-menu-body"></div>
+</div>
+<header class="site-navbar container py-0 " role="banner">
 
-    <h1><?= esc($title) ?></h1>
+<div class="row align-items-center">
+<div class="col-6 col-xl-2">
+<h1 class="mb-0 site-logo"><a href="index-2.html" class="text-white mb-0">DirectoryAds</a></h1>
+</div>
+<div class="col-12 col-md-10 d-none d-xl-block">
+<nav class="site-navigation position-relative text-right" role="navigation">
+<ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+<li class="active"><a href="index-2.html">Home</a></li>
+<li><a href="listings.html">Ads</a></li>
+<li class="has-children">
+<a href="about.html">About</a>
+<ul class="dropdown">
+<li><a href="#">The Company</a></li>
+<li><a href="#">The Leadership</a></li>
+<li><a href="#">Philosophy</a></li>
+<li><a href="#">Careers</a></li>
+</ul>
+</li>
+<li><a href="blog.html">Blog</a></li>
+<li class="mr-5"><a href="contact.html">Contact</a></li>
+<li class="ml-xl-3 login"><a href="login.html"><span class="border-left pl-xl-4"></span>Log In</a></li>
+<li><a href="register.html" class="cta"><span class="bg-primary text-white rounded">Register</span></a></li>
+</ul>
+</nav>
+</div>
+<div class="d-inline-block d-xl-none ml-auto py-3 col-6 text-right" style="position: relative; top: 3px;">
+<a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a>
+</div>
+</div>
+
+</header>
